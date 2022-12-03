fccd4e55950f5d182b8de43f2d5c4e99c6c9dbb7
arthurkennedy <31826050+arthurkennedy@users.noreply.github.com>
2022-12-02 21:43:26 -0600

    Create Soldier.java

---
+++ Soldier.java
@@ -0,0 +1,65 @@
+
+
+/**
+ *
+ * @author arthu
+ */
+
+public class Soldier extends Survivor {
+    
+    int soldierQuantity;
+
+    @Override
+    public int getSurvivorQuantity() {
+        return soldierQuantity;
+    }
+
+    @Override
+    public String getType(int[] survivor) {
+        return survivorType;
+    }
+
+    @Override
+    public void setType(int[] survivor) {
+        survivorType = "Soldier";
+    }
+
+    @Override
+    public void setHealth(int[] survivor) {
+        health = 100;
+    }
+
+    @Override
+    public int getHealth(int survivorType) {
+        return health;
+    }
+
+    @Override
+    public void setAttack(int attack) {
+        attack = 10;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    @Override
+    public boolean isDead(int[] survivor) {
+        boolean isDead;
+        if (health <= 0) {
+            isDead = true;
+            soldierQuantity --; //Decrement the quantity of zombies by one if zombie is dead.
+        } else {
+            isDead = false;
+        }
+        return isDead;
+    }
+
+    @Override
+    public int takeDmg(int[] survivor, int dmgReceived) {
+        health -= dmgReceived;
+        return health;
+    }
+    
+}
