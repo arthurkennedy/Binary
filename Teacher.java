6b5e9fa70c0cee92189864376a0d6cfed50dbb27
arthurkennedy <31826050+arthurkennedy@users.noreply.github.com>
2022-12-02 21:44:18 -0600

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,66 @@
+
+
+/**
+ *
+ * @author arthu
+ */
+
+
+public class Teacher extends Survivor {
+    
+    int teacherQuantity;
+
+    @Override
+    public int getSurvivorQuantity() {
+        return teacherQuantity;
+    }
+
+    @Override
+    public String getType(int[] survivor) {
+        return survivorType;
+    }
+
+    @Override
+    public void setType(int[] survivor) {
+        survivorType = "Teacher";
+    }
+
+    @Override
+    public void setHealth(int[] survivor) {
+        health = 50;
+    }
+
+    @Override
+    public int getHealth(int survivorType) {
+        return health;
+    }
+
+    @Override
+    public void setAttack(int attack) {
+        attack = 5;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    @Override
+    public boolean isDead(int[] survivor) {
+        boolean isDead;
+        if (health <= 0) {
+            isDead = true;
+            teacherQuantity --; //Decrement the quantity of zombies by one if zombie is dead.
+        } else {
+            isDead = false;
+        }
+        return isDead;
+    }
+
+    @Override
+    public int takeDmg(int[] survivor, int dmgReceived) {
+        health -= dmgReceived;
+        return health;
+    }
+    
+}
